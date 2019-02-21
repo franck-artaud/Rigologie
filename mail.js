72366bc573d15d4786bfcc9c3bcfe24bbedc747d
franck-artaud <franck-artaud@atelier601.com>
2019-02-21 15:20:08 +0100

    formulaire réparé

--- mail.js
+++ mail.js
@@ -2,22 +2,27 @@ $(document).ready(function() {
 
     $('#contactForm').on('submit', function(e){
         e.preventDefault();
-        $.ajax({
-            type: 'POST',
-            url: 'mail.php',
-            data: $(this).serialize(),
-            success: function(res){
-                if(res == 'Success'){
-                    $(this).find('input').val('');
-                    $(this).find('textarea').val('');
-                    $('#submitResult').append('<div class="alert success"><i class="fas fa-check"></i> Votre message a bien été envoyé</div>');
-                    $('#contactForm').trigger('reset');
-                } else {
-                    $('#submitResult').append('<div class="alert error"><i class="fas fa-times"></i> Une erreur est survenue, réessayer s\'il vous plait</div>');
-                }
-            }
-        })
-        return false;
+        var data = {
+            lastName: $(this.lastName).val(),
+            firstName:$(this.firstName).val(),
+            emailAdress: $(this.emailAdress).val(),
+            phone: $(this.phone).val(),
+            type: $(this.type).val(),
+            entreprise: $(this.entreprise).val(),
+            adress: $(this.adress).val(),
+            city: $(this.city).val(),
+            cp: $(this.cp).val(),
+            message: $(this.message).val()
+        };
+        Email.send({
+            SecureToken : "048f4df7-bddd-43f7-8bb4-634e4ed5249f",
+            To : 'evolutionpositive85@gmail.com',
+            From : "contact@evolutionpositive.fr",
+            Subject : "Nouveau message de evolutionpositive.fr",
+            Body : data.lastName + ' ' + data.firstName + '<br /><br />' + data.phone + '<br />' + data.emailAdress + '<br />' + data.adress + ', ' + data.cp + ' ' + data.city + '<br />' + data.type + ' - ' + data.entreprise + '<br /><br />' + data.message
+        }).then(
+          message => message === 'OK' ? alert('Votre message à bien été envoyé.') : alert('Une erreur est survenue, veuillez réesayez')
+        );
     });
 
 });
